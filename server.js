e9ea7472d7d71910cb5a4b5a4cbce06a82ab408d
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-01-09 20:55:23 +0100

    add main.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+let clients = 0;
+
+io.on('connection', function(socket){
+  socket.on('NewClient', function(){
+    if(clients < 2){
+      if(clients === 1){
+        this.emit('CreatePeer')
+      }
+    }
+  })
+})
