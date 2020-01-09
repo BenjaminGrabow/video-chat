b07e12ede5eef8c66a388f950bf612bf49313011
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-01-09 21:01:10 +0100

    add server

--- server.js
+++ server.js
@@ -14,5 +14,28 @@ io.on('connection', function(socket){
         this.emit('CreatePeer')
       }
     }
+    else {
+      this.emit('SessionActive')
+      clients++;
+    }
   })
+  socket.on('Offer', SendOffer);
+  socket.on('Answer', SendAnswer);
+  socket.on('disconnect', Disconnect);
 })
+
+function Disconnect() {
+  if (clients > 0)
+  clients--;
+}
+
+function SendOffer(offer){
+  this.broadcast.emit('BackOffer', offer);
+}
+
+
+function SendAnswer(data){
+  this.broadcast.emit('BackAnswer', data);
+}
+
+http.listen(port, () => console.log(`Active on ${port} port`));
